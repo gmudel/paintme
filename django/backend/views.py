d11d9af9bee57f30a67cb5d5b4236756b156d9e3
Gabriel Mudel <gmudel@hotmail.com>
2020-12-14 04:23:11 -0500

    implemented artist endpoint

--- django/backend/views.py
+++ django/backend/views.py
@@ -7,28 +7,45 @@ import os
 from random import sample
 import base64
 
+def get_encoded_img(filename):
+    filepath = os.path.join(path, filename)
+    with open(filepath, mode='rb') as img_file:
+        img = img_file.read()
+        return base64.encodebytes(img).decode('utf-8')
+
 # returns 10 random images from the pool of style images
 def random(request):
-    if request != "GET":
+    if request.method != "GET":
         return HttpResponseNotAllowed(("GET",))
+    resp = {'imgs': []}
     static_root = settings.STATIC_ROOT
     path = os.path.join(static_root, "vincentvangogh")
 
-    resp = {'imgs': []}
     filenames = sample(os.listdir(path), 10)
     for filename in filenames:
-        filepath = os.path.join(path, filename)
-        with open(filepath, mode='rb') as img_file:
-            img = img_file.read()
-            encoded_img = base64.encodebytes(img).decode('utf-8')
-            resp['imgs'].append(encoded_img)
+        encoded_img = get_encoded_img(filename)
+        resp['imgs'].append(encoded_img)
 
     data = json.dumps(resp)
     return HttpResponse(data, content_type='application/json')
 
 def artist(request, artist, offset):
+    if request.method != "GET":
+        return HttpResponseNotAllowed(("GET",))
+    resp = {'imgs': []}
+    static_root = settings.STATIC_ROOT
+    path = os.path.join(static_root, artist)
 
-    return
+    all_filenames = os.listdir(path)
+    startrange = offset * 10
+    endrange = min(len(all_filenames), startrange + 10)
+    selected_filenames = all_filenames[startrange:endrange]
+
+    for filename in selected_filenames:
+        encoded_img = get_encoded_img(filename)
+        resp['imgs'].append(encoded_img)
+    data = json.dumps(resp)
+    return HttpResponse(data, content_type='application/json')
 
 def paintme(request, style_img, content_img):
     return
